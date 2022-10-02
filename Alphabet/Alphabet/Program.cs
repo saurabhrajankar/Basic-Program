8cf026a356dfd4f579e06d33d96c701ae7194a6a
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-03 00:37:44 +0530

    Add files via upload

---
+++ Alphabet/Alphabet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the character to be check:");
+            Char chToCheck = Console.ReadLine()[0];
+            Console.Write(chToCheck);
+            
+            if (chToCheck == 'a' || chToCheck == 'e' || chToCheck == 'i' || 
+                chToCheck == 'o' || chToCheck == 'u' || chToCheck == 'A' || 
+                chToCheck == 'E' || chToCheck == 'I' || chToCheck == 'O' || 
+                chToCheck == 'U')
+                Console.WriteLine("The entered character is a vowel");
+            else
+                Console.WriteLine("The entered character is  a Consonant");
+                Console.ReadLine();
+        }
+        
+
+    }
+}   
