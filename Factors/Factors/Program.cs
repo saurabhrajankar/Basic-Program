0150b29a85b9aca0acb8afb353bb2e2659493fbf
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-03 00:44:25 +0530

    Add files via upload

---
+++ Factors/Factors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your integer number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine($"{i} is a prime factor of {num}");
+                }
+            }
+            Console.ReadLine();
+        }   
+    }
+}
