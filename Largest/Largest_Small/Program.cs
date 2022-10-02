2ed7a34fb25d0092ae45e68f8ab262472b7d3843
saurabhrajankar <saurabhrajankar777@gmail.com>
2022-10-03 00:28:12 +0530

    Largest Num

---
+++ Largest/Largest_Small/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Small
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter user three numbers");
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            int c=Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"{a}a is the largest number");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine($"{b} is the largest number");
+            }
+            else
+            {
+                Console.WriteLine($"{c} is the largest number");
+            }
+            Console.ReadLine();
+                            
+
+        }
+    }
+}
